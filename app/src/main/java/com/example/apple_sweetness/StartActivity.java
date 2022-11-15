2ff276ee7c0889406ca8f1d4464fdcbc181d7e21
hanbikim <hanbikim13@gmail.com>
2022-11-15 16:48:48 -0500

    Feat: start, main, camera

---
+++ app/src/main/java/com/example/apple_sweetness/StartActivity.java
@@ -0,0 +1,26 @@
+package com.example.apple_sweetness;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Handler;
+
+public class StartActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(StartActivity.this, MainActivity.class);
+                startActivity(intent);
+                overridePendingTransition(0, 0);
+            }
+        }, 1500);
+
+    }
+}
