daf6742ac571013be342fade16f5c8b2a57469b6
hanbikim <hanbikim13@gmail.com>
2022-11-16 11:19:49 -0500

    Feat: camera high resultion

--- app/src/main/java/com/example/apple_sweetness/CameraActivity.java
+++ app/src/main/java/com/example/apple_sweetness/CameraActivity.java
@@ -4,16 +4,27 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.ImageDecoder;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.FileProvider;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class CameraActivity extends AppCompatActivity {
     final private static String TAG = "Top Gun";
@@ -40,12 +51,12 @@ public class CameraActivity extends AppCompatActivity {
                 ActivityCompat.requestPermissions(CameraActivity.this, new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
             }
         }
+        dispatchTakePictureIntent();
 
-        Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(cameraIntent, TAKE_PICTURE);
+//        Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+//        startActivityForResult(cameraIntent, TAKE_PICTURE);
 
     }
-
 
     // 권한 요청
     @Override
@@ -58,21 +69,77 @@ public class CameraActivity extends AppCompatActivity {
     }
 
 
-    // 카메라로 촬영한 사진의 썸네일을 가져와 이미지뷰에 띄워줌
+    // 카메라로 촬영한 영상을 가져오는 부분
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
         super.onActivityResult(requestCode, resultCode, intent);
-
-        switch (requestCode) {
-            case TAKE_PICTURE:
-                if (resultCode == RESULT_OK && intent.hasExtra("data")) {
-                    Bitmap bitmap = (Bitmap) intent.getExtras().get("data");
-                    if (bitmap != null) {
-                        iv_photo.setImageBitmap(bitmap);
+        try {
+            switch (requestCode) {
+                case REQUEST_TAKE_PHOTO: {
+                    if (resultCode == RESULT_OK) {
+                        File file = new File(mCurrentPhotoPath);
+                        Bitmap bitmap;
+                        if (Build.VERSION.SDK_INT >= 29) {
+                            ImageDecoder.Source source = ImageDecoder.createSource(getContentResolver(), Uri.fromFile(file));
+                            try {
+                                bitmap = ImageDecoder.decodeBitmap(source);
+                                if (bitmap != null) { iv_photo.setImageBitmap(bitmap); }
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        } else {
+                            try {
+                                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), Uri.fromFile(file));
+                                if (bitmap != null) { iv_photo.setImageBitmap(bitmap); }
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
                     }
+                    break;
                 }
-                break;
+            }
+        } catch (Exception error) {
+            error.printStackTrace();
         }
     }
+
+    // 사진 촬영 후 썸네일만 띄워줌. 이미지를 파일로 저장해야 함
+    private File createImageFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,
+                ".jpg",
+                storageDir
+        );
+
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    // 카메라 인텐트 실행하는 부분
+    private void dispatchTakePictureIntent() {
+//        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//        startActivityForResult(cameraIntent, TAKE_PICTURE);
+
+        Intent takePictureIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+
+        if(getApplicationContext().getPackageManager().hasSystemFeature(
+                PackageManager.FEATURE_CAMERA)) {
+            File photoFile = null;
+
+            try { photoFile = createImageFile(); }
+            catch (IOException ex) { }
+            if(photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this, "com.example.FileProvider", photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+   }
+
+
 }
 
--- app/src/main/java/com/example/apple_sweetness/StartActivity.java
+++ app/src/main/java/com/example/apple_sweetness/StartActivity.java
@@ -24,3 +24,4 @@ public class StartActivity extends AppCompatActivity {
 
     }
 }
+
