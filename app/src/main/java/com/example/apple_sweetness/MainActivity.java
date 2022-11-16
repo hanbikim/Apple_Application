9ce72f52ea0a6ac938017514cd171b7443a4da58
Hanbi-Kim <74577775+hanbikim@users.noreply.github.com>
2022-11-16 18:42:38 -0500

    Merge; Hanbi & Kamtae (#9)
    * sever python code
    
    * Feat: start, main, camera
    
    * Feat: camera high resultion
    
    * Feat: camera high resultion
    
    * Feat: album

--- app/src/main/java/com/example/apple_sweetness/MainActivity.java
+++ app/src/main/java/com/example/apple_sweetness/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.apple_sweetness;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,23 +9,69 @@ import androidx.appcompat.widget.AppCompatButton;
 
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
 
 
+    private static final int REQUEST_CODE = 0;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        AppCompatButton btn = findViewById(R.id.Camera);
-        btn.setOnClickListener(new View.OnClickListener() {
+        //Camera
+        Button CameraBtn = findViewById(R.id.Camera);
+        CameraBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), CameraActivity.class);
-                startActivity(intent);
+                Intent CameraIntent = new Intent(getApplicationContext(), CameraActivity.class);
+                startActivity(CameraIntent);
             }
         });
+
+        //Album, Gallery
+        Button AlbumBtn = findViewById(R.id.Album);
+        AlbumBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.activity_photo);
+                imageView = findViewById(R.id.iv_photo);
+
+                ImagefromGallery();
+//                Intent AlbumIntent = new Intent(getApplicationContext(), AlbumActivity.class);
+//                startActivity(AlbumIntent);
+            }
+
+        });
+    }
+
+    private void ImagefromGallery() {
+        Intent AlbumIntent = new Intent();
+        AlbumIntent.setType("image/*");
+        AlbumIntent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(AlbumIntent, REQUEST_CODE);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                try{
+                    Uri uri = data.getData();
+                    Glide.with(getApplicationContext()).load(uri).into(imageView);
+                }catch (Exception e){
+
+                }
+            }else if(resultCode ==RESULT_CANCELED){ //cancel code!!!
+
+            }
+        }
     }
 }
