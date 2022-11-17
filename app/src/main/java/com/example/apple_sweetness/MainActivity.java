145b5c666a7c7d1f1b2e7e7c5f80ae670df0fbc1
KAMTAE <persimm0ncrack@gmail.com>
2022-11-17 10:13:45 -0500

    select_image

--- app/src/main/java/com/example/apple_sweetness/MainActivity.java
+++ app/src/main/java/com/example/apple_sweetness/MainActivity.java
@@ -1,20 +1,77 @@
 package com.example.apple_sweetness;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
 
 
+    private static final int REQUEST_CODE = 0;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_select);
+        setContentView(R.layout.activity_main);
+
+        //Camera
+        Button CameraBtn = findViewById(R.id.Camera);
+        CameraBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent CameraIntent = new Intent(getApplicationContext(), CameraActivity.class);
+                startActivity(CameraIntent);
+            }
+        });
+
+        //Album, Gallery
+        Button AlbumBtn = findViewById(R.id.Album);
+        AlbumBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.activity_photo);
+                imageView = findViewById(R.id.iv_photo);
+
+                ImagefromGallery();
+//                Intent AlbumIntent = new Intent(getApplicationContext(), AlbumActivity.class);
+//                startActivity(AlbumIntent);
+            }
+
+        });
+    }
+
+    private void ImagefromGallery() {
+        Intent AlbumIntent = new Intent();
+        AlbumIntent.setType("image/*");
+        AlbumIntent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(AlbumIntent, REQUEST_CODE);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                try{
+                    Uri uri = data.getData();
+                    Glide.with(getApplicationContext()).load(uri).into(imageView);
+                }catch (Exception e){
+
+                }
+            }else if(resultCode ==RESULT_CANCELED){ //cancel code!!!
+
+            }
+        }
     }
 }
