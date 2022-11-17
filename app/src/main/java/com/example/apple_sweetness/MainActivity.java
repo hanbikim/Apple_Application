c8d73bfa5f86e661e9c9c61ea50b63c8587a262a
KAMTAE <persimm0ncrack@gmail.com>
2022-11-17 09:39:59 -0500

    select_image

--- app/src/main/java/com/example/apple_sweetness/MainActivity.java
+++ app/src/main/java/com/example/apple_sweetness/MainActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.AppCompatButton;
 
 import android.view.View;
 import android.widget.Button;
@@ -16,15 +15,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        AppCompatButton btn = findViewById(R.id.Camera);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), CameraActivity.class);
-                startActivity(intent);
-            }
-        });
+        setContentView(R.layout.activity_select);
     }
 }
