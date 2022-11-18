f75988b37fb0a2aee8d700441d414827fdc49a63
Hanbi-Kim <74577775+hanbikim@users.noreply.github.com>
2022-11-18 14:47:07 -0500

    image external storage to local storage (#20)
    * sever python code
    
    * Feat: start, main, camera
    
    * Feat: camera high resultion
    
    * Feat: camera high resultion
    
    * Feat: album
    
    * Delete AlbumActicity
    
    * comment in eng
    
    * apply activity_select
    
    * back, start textview
    
    * connect ShowImagesActivity & xml
    
    * storage img in local

--- app/src/main/java/com/example/apple_sweetness/MainActivity.java
+++ app/src/main/java/com/example/apple_sweetness/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.apple_sweetness;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.AppCompatButton;
 
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -14,6 +18,7 @@ import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -51,7 +56,6 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-
     //Album
     private void ImagefromGallery() {
         Intent AlbumIntent = new Intent();
@@ -60,6 +64,7 @@ public class MainActivity extends AppCompatActivity {
         startActivityForResult(AlbumIntent, REQUEST_CODE);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -70,12 +75,42 @@ public class MainActivity extends AppCompatActivity {
                     Uri uri = data.getData();
                     Glide.with(getApplicationContext()).load(uri).into(imageView);
                 }catch (Exception e){
-
                 }
             }else if(resultCode ==RESULT_CANCELED){ //cancel code
 
             }
         }
+    }
+
+    public String getImageNameToUri(Uri data)
+    {
+        String[] proj = { MediaStore.Images.Media.DATA };
+        Cursor cursor = managedQuery(data, proj, null, null, null);
+        int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+
+        cursor.moveToFirst();
+
+        String imgPath = cursor.getString(column_index);
+        String imgName = imgPath.substring(imgPath.lastIndexOf("/")+1);
+
+        return imgName;
+    }
+
+    public String getImageFilePath(Uri uri) {
+
+        File file = new File(uri.getPath());
+        String[] filePath = file.getPath().split(":");
+        String image_id = filePath[filePath.length - 1];
+
+        Cursor cursor = getContentResolver().query(android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI, null, MediaStore.Images.Media._ID + " = ? ", new String[]{image_id}, null);
+        if (cursor != null) {
+            cursor.moveToFirst();
+            @SuppressLint("Range") String imagePath = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
+
+            cursor.close();
+            return imagePath;
+        }
+        return null;
     }
 
     private void textViewClick(){
