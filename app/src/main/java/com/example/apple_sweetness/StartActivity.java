1598820e6dd16ef06efd731cc53556a7f5f8a347
hanbikim <hanbikim13@gmail.com>
2022-11-28 14:45:10 -0500

    img: server to android

--- app/src/main/java/com/example/apple_sweetness/StartActivity.java
+++ app/src/main/java/com/example/apple_sweetness/StartActivity.java
@@ -16,7 +16,7 @@ public class StartActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(StartActivity.this, MainActivity.class);
+                Intent intent = new Intent(StartActivity.this, ShowImagesActivity.class);
                 startActivity(intent);
                 overridePendingTransition(0, 0);
             }
