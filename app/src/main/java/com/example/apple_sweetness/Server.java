1b5315841482c2fd2689e17cc39ddb49899c4e71
Hanbi-Kim <74577775+hanbikim@users.noreply.github.com>
2022-11-28 16:57:50 -0500

    show text (#26)
    * sever python code
    
    * Feat: start, main, camera
    
    * Feat: camera high resultion
    
    * Feat: camera high resultion
    
    * Feat: album
    
    * Delete AlbumActicity
    
    * comment in eng
    
    * apply activity_select
    
    * back, start textview
    
    * connect ShowImagesActivity & xml
    
    * storage img in local
    
    * fix: album bug, camera crash
    
    * img: server to android
    
    * show text

--- app/src/main/java/com/example/apple_sweetness/Server.java
+++ app/src/main/java/com/example/apple_sweetness/Server.java
@@ -1,6 +1,7 @@
 package com.example.apple_sweetness;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -25,16 +26,18 @@ import okhttp3.Response;
 public class Server extends AppCompatActivity {
 
     // declare attribute for textview
+    private EditText editText;
     private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.test);
+        //editText = findViewById(R.id.text);
         textView = findViewById(R.id.text);
 
         OkHttpClient okHttpClient = new OkHttpClient();
-        Request request = new Request.Builder().url("http://192.168.2.214:5002/").build();
+        Request request = new Request.Builder().url("http://192.168.2.173:777/txt").build();
         okHttpClient.newCall(request).enqueue(new Callback() {
             @Override
             public void onFailure(@NonNull Call call, @NonNull IOException e) {
@@ -42,14 +45,23 @@ public class Server extends AppCompatActivity {
                     @Override
                     public void run() {
                         Toast.makeText(Server.this, "server down", Toast.LENGTH_SHORT).show();
-                        textView.setText("error connecting to the server");
                     }
                 });
             }
 
             @Override
             public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
-                textView.setText(response.body().string());
+
+               String sugarLevels = response.body().string();
+
+                String sugarLevel[] = sugarLevels.split(",");
+                StringBuffer sugarText = new StringBuffer();
+
+                for(int i = 0; i<sugarLevel.length; i++){
+                    sugarText.append(i+1);
+                    sugarText.append(". "+sugarLevel[i]+"\n");
+                }
+                textView.setText(sugarText);
             }
         });
     }
