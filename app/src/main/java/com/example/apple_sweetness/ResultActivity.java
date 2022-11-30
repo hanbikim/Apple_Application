a1c09d97b87d1c7987876d99008c94e061b4bf87
Hanbi-Kim <74577775+hanbikim@users.noreply.github.com>
2022-11-29 22:59:42 -0500

    sever function (#31)
    * sever python code
    
    * Feat: start, main, camera
    
    * Feat: camera high resultion
    
    * Feat: camera high resultion
    
    * Feat: album
    
    * Delete AlbumActicity
    
    * comment in eng
    
    * apply activity_select
    
    * back, start textview
    
    * connect ShowImagesActivity & xml
    
    * storage img in local
    
    * fix: album bug, camera crash
    
    * img: server to android
    
    * show text
    
    * image center align
    
    * sever function

--- app/src/main/java/com/example/apple_sweetness/ResultActivity.java
+++ app/src/main/java/com/example/apple_sweetness/ResultActivity.java
@@ -1,6 +1,8 @@
 package com.example.apple_sweetness;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.os.Handler;
 import android.text.method.ScrollingMovementMethod;
@@ -8,15 +10,33 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 public class ResultActivity extends AppCompatActivity {
+
+    Bitmap bitmap;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_apple_sweetness);
+
+        ImageView imageView = (ImageView)findViewById(R.id.appleImg2);
 
         //scroll
         TextView apple_result = (TextView)findViewById(R.id.apple_result);
@@ -24,26 +44,95 @@ public class ResultActivity extends AppCompatActivity {
 
 
         //Home button
-
-      Button home = findViewById(R.id.home_bu);
-       home.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View v) {Intent AppleSweetnessIntent = new Intent(getApplicationContext(), MainActivity.class);
-              startActivity(AppleSweetnessIntent);
-            }
+        Button home = findViewById(R.id.home_bu);
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {Intent AppleSweetnessIntent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(AppleSweetnessIntent);
+                }
         });
 
+
+
+        //wait
+
+        // get_server()
+        //sugar levels - text
+        getSugarLevels(apple_result);
+        //apple image
+        getAppleImage(imageView);
+
+
+
+    }
+
+
+
+
+    private void getSugarLevels(TextView textView){
+        OkHttpClient okHttpClient = new OkHttpClient();
+        Request request = new Request.Builder().url("http://192.168.2.173:777/txt").build();
+        okHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(ResultActivity.this, "server down", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+
+                String sugarLevels = response.body().string();
+
+                String sugarLevel[] = sugarLevels.split(",");
+                StringBuffer sugarText = new StringBuffer();
+
+                for(int i = 0; i<sugarLevel.length; i++){
+                    sugarText.append(i+1);
+                    sugarText.append(". "+sugarLevel[i]+"\n");
+                }
+                textView.setText(sugarText);
+            }
+        });
+    }
+
+
+    private void getAppleImage(ImageView imageView){
+        Thread mThread = new Thread(){
+            @Override
+            public void run(){
+                try{
+                    URL url = new URL("http://192.168.2.173:777/static/squared_tree/detected_apples.jpg");
+
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    conn.setDoInput(true);
+                    conn.connect();
+
+                    InputStream is = conn.getInputStream();
+                    bitmap = BitmapFactory.decodeStream(is);
+                } catch (MalformedURLException e){
+                    e.printStackTrace();
+                } catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+        };
+        mThread.start();
+
+        try {
+            mThread.join();
+
+            imageView.setImageBitmap(bitmap);
+        } catch (InterruptedException e){
+            e.printStackTrace();
+        }
     }
 
 
 }
 
-//img -> server
 
-
-//server -> img
-
-
-
-
-// server -> result txt
